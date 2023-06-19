6a5763ca4bac2b7bcbfc8076458c3a7c4bf88eb0
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2023-06-19 11:08:34 +0530

    uses subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { OnInit, OnDestroy } from '@angular/core';
+
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'observables-section';
+export class AppComponent implements OnInit, OnDestroy{
+  
+  private obsSubscription: Subscription;
+  constructor(private userService: UserService){}
+
+  isActivated: boolean = false;
+
+  ngOnInit(): void {
+      
+    this.obsSubscription =  this.userService.activatedEmitter.subscribe((activateUser: boolean) => {
+      console.log("Hii Aman!");
+      this.isActivated = activateUser;
+    })
+
+  }
+
+  ngOnDestroy(): void {
+      this.obsSubscription.unsubscribe();
+  }
 }
